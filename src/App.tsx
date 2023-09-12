08484ddc17eb6f1e12c7ba5cc18e6722be06498a
Troy Tran <troy.tran.goldenowl@gmail.com>
2023-09-12 14:03:01 +0700

    feat: initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import {
   GitHubBanner,
   WelcomePage,
   Authenticated,
-} from "@refinedev/core";
-import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+} from '@refinedev/core'
+import { RefineKbar, RefineKbarProvider } from '@refinedev/kbar'
 
 import {
   AuthPage,
@@ -12,18 +12,18 @@ import {
   notificationProvider,
   RefineThemes,
   ThemedLayoutV2,
-} from "@refinedev/chakra-ui";
+} from '@refinedev/chakra-ui'
 
-import { ChakraProvider } from "@chakra-ui/react";
-import { BrowserRouter, Route, Routes, Outlet } from "react-router-dom";
+import { ChakraProvider } from '@chakra-ui/react'
+import { BrowserRouter, Route, Routes, Outlet } from 'react-router-dom'
 import routerBindings, {
   NavigateToResource,
   CatchAllNavigate,
   UnsavedChangesNotifier,
   DocumentTitleHandler,
-} from "@refinedev/react-router-v6";
-import dataProvider from "@refinedev/simple-rest";
-import { Header } from "./components/header";
+} from '@refinedev/react-router-v6'
+import dataProvider from '@refinedev/simple-rest'
+import { Header } from './components/header'
 
 function App() {
   return (
@@ -31,15 +31,15 @@ function App() {
       <GitHubBanner />
       <RefineKbarProvider>
         {/* You can change the theme colors here. example: theme={RefineThemes.Magenta} */}
-        <ChakraProvider theme={RefineThemes.Blue}>
+        <ChakraProvider theme={RefineThemes.Yellow}>
           <Refine
             notificationProvider={notificationProvider}
             routerProvider={routerBindings}
-            dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+            dataProvider={dataProvider('https://api.fake-rest.refine.dev')}
             options={{
               syncWithLocation: true,
               warnWhenUnsavedChanges: true,
-              projectId: "ab0GnW-jpc5vd-wtKeUk",
+              projectId: 'ab0GnW-jpc5vd-wtKeUk',
             }}
           >
             <Routes>
@@ -52,7 +52,7 @@ function App() {
         </ChakraProvider>
       </RefineKbarProvider>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
